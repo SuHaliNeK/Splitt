f125c60cccbc873f5c79d87a0267a771206a0934
r <user.name>
2018-05-15 18:41:23 +0200

    Add Bills model, Bills controller and index action

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Bill.create(name:"Pizza", price:100)
+Bill.create(name:"Hamburger", price:20)
+Bill.create(name:"Fish", price:15)
+Bill.create(name:"Fries", price:7)
+Bill.create(name:"FastFood", price:30)
+puts "Bills created"
