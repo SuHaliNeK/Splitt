2e2542cadee40bbefc79bcf89c3d09cf9f6026d9
r <user.name>
2018-05-29 18:16:30 +0200

    Show view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,21 @@
-Bill.create(name:"Pizza", price:100)
-Bill.create(name:"Hamburger", price:20)
-Bill.create(name:"Fish", price:15)
-Bill.create(name:"Fries", price:7)
-Bill.create(name:"FastFood", price:30)
+Recipient.destroy_all
+Bill.destroy_all
+
+
+
+
+bill1 = Bill.create(name:"Pizza", price:100, bank_account:"1234", description: "opis1" )
+bill2 = Bill.create(name:"Hamburger", price:20, bank_account:"4321", description: "opis2")
+bill3 = Bill.create(name:"Fish", price:15, bank_account:"1243", description: "opis3")
+
+bill1.recipients.create(name: "Patryk", email: "pati102@wp.pl")
+bill1.recipients.create(name: "Adam", email: "adam102@wp.pl")
+
+bill2.recipients.create(name: "Piter", email: "piter102@wp.pl")
+bill2.recipients.create(name: "Miki", email: "miki@wp.pl")
+
+bill3.recipients.create(name: "Tomek", email: "tomek@wp.pl")
+bill3.recipients.create(name: "Krzyś", email: "krzys@wp.pl")
+
+
 puts "Bills created"
